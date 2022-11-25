fead0acd8d5585e0658695bcf958f4f46ee9fc50
lucasyasonoda <sonodaprog@gmail.com>
2022-11-25 16:01:36 -0300

    Update App.java

--- SchoolTeacher/school/src/App.java
+++ SchoolTeacher/school/src/App.java
@@ -15,13 +15,13 @@ public class App {
                     subjects.add(RegisterSub());
                     break;
                 case 2:
-                    Search(subjects);
+                    SearchSub(subjects);
                     break;
                 case 3:
-
+                    SearchTitle(subjects);
                     break;
                 case 4:
-
+                    SearchTeachers(subjects);
                     break;
                 case 5:
                     System.exit(0);
@@ -64,18 +64,41 @@ public class App {
         return s; 
     }
 
-    public static void Search(ArrayList<Subject> subs){
+    public static void SearchSub(ArrayList<Subject> subs){
         var scan = new Scanner(System.in);
         String sub;
 
-        System.out.println(">>> Search subject <<<");
+        System.out.println(">>> Search teacher by subject <<<");
         System.out.println(">> Insert the subject: ");
             sub = scan.nextLine();
 
         for(int i=0;i<subs.size();i++)
             if(subs.get(i).getName().equals(sub))
-                System.out.println("The teacher's name is " + subs.get(i).getTeacher().getName());
-            
-        
+                System.out.println("The teacher's name is " + subs.get(i).getTeacher().getName());                    
+    }
+
+    public static void SearchTitle(ArrayList<Subject> subs){
+        var scan = new Scanner(System.in);
+        String title;   // Can be "Metre, Doutor, Especialista"
+        int cond=0;
+
+        System.out.println(">>> Search teacher by title <<<");
+        System.out.println(">> Insert the title: ");
+            title = scan.nextLine();
+
+        System.out.println("The teache's name who has title of " + title); 
+        for(int i=0;i<subs.size();i++)
+            if(subs.get(i).getTeacher().getTitulation().equals(title)){
+                System.out.println("["+i+"] Name: " + subs.get(i).getTeacher().getName());
+                cond++;
+            }
+        if(cond == 0)
+            System.out.println("> No teacher has this title!!! <");            
+    }
+
+    public static void SearchTeachers(ArrayList<Subject> subs){
+        for(var sub: subs)
+            if(sub.getTeacher().getName() != "")
+                System.out.println("The " + sub.getName() + " subject has at least 1 teacher");               
     }
 }
